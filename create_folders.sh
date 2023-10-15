a4976ac91e7bd99ea298577da7112da469b48d57
Zixuan Zhang <zixuan11@illinois.edu>
2023-10-14 23:05:59 -0500

    Create create_folders.sh

---
+++ create_folders.sh
@@ -0,0 +1,12 @@
+# note: first cd into KAIROS_LIB before running this script!
+mkdir resin
+cd resin
+mkdir resin
+cd resin
+mkdir input
+mkdir persist
+mkdir schemas
+mkdir log
+cd schemas
+mkdir resin
+mkdir isi
